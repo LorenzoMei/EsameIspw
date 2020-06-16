d533324a576a1a2e6955fe68a8f07d731dd85419
LorenzoMei <45484506+LorenzoMei@users.noreply.github.com>
2020-06-16 16:20:36 +0200

    #1

--- src/logic/StringIspw.java
+++ src/logic/StringIspw.java
@@ -8,9 +8,14 @@ public class StringIspw {
 		
 		String resultConc = conc(stringOne, stringTwo, "-");
 		String resultFormat = format(resultConc, resultConc.length());
-		
+		String[] resultSplit = split(resultConc, "-");
 		System.out.println("Stringhe concatenate: " + resultConc);
 		System.out.println("Stringa formattata: " + resultFormat);
+		
+		for(int i = 0; i < resultSplit.length; i++) {
+			System.out.println("String " + i + ": " + resultSplit[i]);
+		}
+		
 	}
 	
 	public static String conc(String stringOne, String stringTwo, String delimiter) {
@@ -21,6 +26,10 @@ public class StringIspw {
 	public static String format(String string, int i) {
 		String formatString = String.format(string + ". Lettere contenute: %d", i);
 		return formatString;
+	}
+	
+	public static String[] split(String string, String splitter) {
+		return string.split(splitter);
 	}
 	
 
