d87c3a5f71fdf1f463b707fb075d716fd3d4612d
LorenzoMei <45484506+LorenzoMei@users.noreply.github.com>
2020-06-16 16:52:49 +0200

    #1

--- src/logic/StringIspw.java
+++ src/logic/StringIspw.java
@@ -1,12 +1,28 @@
 package logic;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
 public class StringIspw {
 
 	public static void main(String[] args) {
-		String stringOne = "Prima string";
-		String stringTwo = "Seconda stringa";
+		String[] string = new String[2];
+				
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader buffer = new BufferedReader(input);
 		
-		String resultConc = conc(stringOne, stringTwo, "-");
+		for(int i = 0; i < 2; i++) {
+			System.out.println("Inserisci la stringa numero " + i + "\n");
+			try {
+				string[i] = buffer.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		String resultConc = conc(string[0], string[1], "-");
 		String resultFormat = format(resultConc, resultConc.length());
 		String[] resultSplit = split(resultConc, "-");
 		System.out.println("Stringhe concatenate: " + resultConc);
